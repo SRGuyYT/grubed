7b7e02cfba765e099e528b7b6f1da69924f14619
SRGuyYT <chapmlinks@gmail.com>
2026-03-30 23:31:20 -0400

    Create eslintrc.js

---
+++ eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    // Add your custom rules here if needed
+  },
+};
